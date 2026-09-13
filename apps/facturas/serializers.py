784ac7834a43d6af8deffd3b784c6c60a3ca404c
gerson <gersoncardena69@gmail.com>
2026-09-13 14:28:52 -0300

    feat: include sale items in invoice serializer

--- apps/facturas/serializers.py
+++ apps/facturas/serializers.py
@@ -3,8 +3,28 @@ from rest_framework import serializers
 from .models import Factura
 
 
-class FacturaSerializer(serializers.ModelSerializer):
+class FacturaItemSerializer(serializers.Serializer):
+    product_nombre = serializers.CharField()
+    bin_nombre = serializers.CharField()
+    cantidad = serializers.IntegerField()
+    bins_cantidad = serializers.IntegerField()
+    tipo_cobro = serializers.CharField()
+    kilos_pesados = serializers.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        allow_null=True,
+    )
+    precio_unitario = serializers.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+    )
+    subtotal = serializers.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+    )
 
+
+class FacturaSerializer(serializers.ModelSerializer):
     sale_numero = serializers.CharField(
         source="sale.numero",
         read_only=True,
@@ -14,6 +34,8 @@ class FacturaSerializer(serializers.ModelSerializer):
         source="sale.estado",
         read_only=True,
     )
+
+    items = serializers.SerializerMethodField()
 
     class Meta:
         model = Factura
@@ -31,6 +53,38 @@ class FacturaSerializer(serializers.ModelSerializer):
             "iva",
             "total",
             "fecha_emision",
+            "items",
         ]
 
         read_only_fields = fields
+
+    def get_items(self, factura):
+        sale_items = (
+            factura.sale.items
+            .select_related(
+                "product",
+                "bin",
+            )
+            .all()
+        )
+
+        data = []
+
+        for item in sale_items:
+            data.append({
+                "product_nombre": item.product.nombre,
+                "bin_nombre": item.bin.nombre,
+                "cantidad": item.cantidad,
+                "bins_cantidad": item.bins_cantidad,
+                "tipo_cobro": item.tipo_cobro_snapshot,
+                "kilos_pesados": item.kilos_pesados,
+                "precio_unitario": item.precio_unitario,
+                "subtotal": item.subtotal,
+            })
+
+        serializer = FacturaItemSerializer(
+            data,
+            many=True,
+        )
+
+        return serializer.data
